572b3c22975ff8293a36b3243b4d91502e88531a
LinickerOstroski <olinicker@gmail.com>
2024-12-27 15:10:08 -0300

    conectando ao banco de dados

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+const db = require('./db/connection')
 
 const PORT = 3000
 app.listen(PORT, function () {
   console.log(`O Express está rodando na porta ${PORT}`)
 })
 
+db
+  .authenticate()
+  .then(() => {
+    console.log('Conectou ao banco com sucesso!')
+  })
+  .catch(err => {
+    console.log('Ocorreu um erro ao conectar')
+  });
+
+//routes
 app.get('/', (req, res) => {
   res.send('Está funcionando')
 })
